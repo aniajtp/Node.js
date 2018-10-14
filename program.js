6f68085a9c89d4b44ecd4e47234d191c2f286782
Anna Półtorak <aniajtp@gmail.com>
2018-10-14 22:48:07 +0200

    poprawiony program.js

--- program.js
+++ program.js
@@ -3,16 +3,15 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    switch(input) {
-        case null:
-          process.stdout.write('Quitting app!');
-          process.exit();
-          break;
-        case '/exit':
-          process.stdout.write('Quitting app!');
-          process.exit();
-          break;
-        default :
-          console.log(process.env);
+    if (input !== null) {
+        var instruction = input.trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!');
+                process.exit();
+             break;
+            default :
+                console.log(process.env);
+        };
     }
 });
